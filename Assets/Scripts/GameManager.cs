bdc86da394e1778784ce42bbc44d54c33e37f444
mromeroamitica <marcos.romero@miticatechnology.com>
2021-04-07 13:37:51 +0200

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+     void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            RaycastHit hit;
+            //El rayo sale de la posicion de la cámara
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            //Si el rayo golpea con un objeto con fisicas, devuelveme el objeto en hit
+            if(Physics.Raycast(ray, out hit))
+            {
+                //Debug.LogError(hit.transform.gameObject.name);
+                hit.transform.gameObject.SetActive(false);
+            }
+
+        }
+    }
+}
